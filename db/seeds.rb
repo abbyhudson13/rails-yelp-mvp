2b3c0dd251b2247537203819483d2f0824cc6c9e
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-19 13:31:05 +0100

    adding models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create(name: 'Yogi Ashram', address: 'Boxhagener Platz', category: 'indian', phone_number: '01578263738')
+restaurants = Restaurant.create(name: 'Soy', address: 'Alexanderplatz', category: 'chinese', phone_number: '01578263998')
+restaurants = Restaurant.create(name: 'Rembrandt Burger', address: 'Weberwiese', category: 'belgian', phone_number: '01578083998')
+restaurants = Restaurant.create(name: 'Chupenga', address: 'Checkpoint Charlie', category: 'mexican', phone_number: '015782927738')
+restaurants = Restaurant.create(name: 'Pizza Point', address: 'Checkpoint Charlie', category: 'italian', phone_number: '018282927738')
