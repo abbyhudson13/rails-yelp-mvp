d0f750be9868142fccf77392ed7deade9d88c977
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-19 15:21:49 +0100

    updating the reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get "restaurants/:restaurant_id/reviews", to: "reviews#index"
+  # get "restaurants/:id", to: "restaurants#show"
+  # get "restaurants/new", to: "restaurants#new"
+  resources :restaurants, only: [ :show, :index, :new, :create] do
+    resources :reviews, only: [ :new, :create ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
